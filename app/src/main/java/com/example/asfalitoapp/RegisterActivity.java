e3efad98bca686dd96e95cd98b158184f4537407
Nrekkas <nrekkas@gmail.com>
2019-12-17 21:21:48 +0200

    Register Try success

--- app/src/main/java/com/example/asfalitoapp/RegisterActivity.java
+++ app/src/main/java/com/example/asfalitoapp/RegisterActivity.java
@@ -1,14 +1,96 @@
 package com.example.asfalitoapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText input_email, input_password, input_passwordver;
+    private Button registerbutton;
+    private TextView redirectbutton;
+    private static final String TAG = "Register";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        input_email = findViewById(R.id.email);
+        input_password = findViewById(R.id.password);
+        input_passwordver = findViewById(R.id.passwordver);
+        registerbutton = findViewById(R.id.registerbtn);
+        redirectbutton = findViewById(R.id.loginredirect);
+
+        redirectbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent myIntent = new Intent(getBaseContext(), LoginActivity.class);
+                    startActivity(myIntent);
+            }
+        });
+
+        registerbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (input_password.getText().toString().equals(input_passwordver.getText().toString())) {
+                    createAccount(input_email.getText().toString(), input_password.getText().toString());
+                }
+            }
+        });
+
+
+    }
+
+    private void createAccount(String email, String password) {
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
     }
 }
